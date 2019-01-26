6bf7472a79b97f3145f0655054852b26a2f47df5
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-01-26 12:07:50 -0700

    app-1 complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      input: '',
+    }
+
+    this.handleUserInput = this.handleUserInput.bind(this)
+
+  }
+
+  handleUserInput(e) {
+    this.setState({
+      input: e,
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <input onChange={ e => this.handleUserInput(e.target.value)}></input>
+
+        <span>{this.state.input}</span>
+
       </div>
     );
   }
