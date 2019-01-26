6a24a7bf0afa1510aca3d4c2971f8e01de40bf8d
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-01-26 14:06:52 -0700

    app-2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      input: '',
+      list: [],
+    }
+
+    this.handleInput = this.handleInput.bind(this)
+    this.handlClick = this.handlClick.bind(this)
+
+  }
+
+  handleInput(e) {
+    this.setState({
+      input: e
+    })
+    console.log(this.state.input)
+  }
+
+  handlClick(value) {
+
+    var newArr = this.state.list
+    newArr.push(value)
+
+    this.setState({
+      list: newArr,
+    })
+    
+  }
+
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+  
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div>
+
+          <input onChange={ (e) => this.handleInput(e.target.value)}></input>
+
+          <button onClick={ () => this.handlClick(this.state.input)}>Add Item</button>
+
+          <list>Array: {this.state.list}</list>
+
+        </div>
+
       </div>
     );
   }
