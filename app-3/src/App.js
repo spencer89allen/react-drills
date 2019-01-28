8056c2e299fc901d3146ab72133599a80df8a792
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-01-27 18:55:41 -0700

    app-3 complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      array: [' Cinnamon Tost Crunch ', ' Wheaties ', ' Lucky Charms ', " Reese's Puffs ", ' Captan Crunch '],
+      filteredArray: [],
+      userInput: '',
+    }
+
+    this.handleUserInput = this.handleUserInput.bind(this)
+    this.handleFilter = this.handleFilter.bind(this)
+    this.handleClear = this.handleClear.bind(this)
+
+  }
+
+  handleUserInput(e) {
+
+    this.setState({
+      userInput: e,
+    })
+
+  }
+
+  handleFilter(userInput) {
+    var filteredArray = this.state.array.filter((value, i, arr) => {
+      return value.toLowerCase().includes(userInput.toLowerCase())
+    })
+
+    this.setState({
+      filteredArray: filteredArray,
+    })
+
+  }
+
+  handleClear() {
+    this.setState({
+      filteredArray: '',
+    })
+  }
+
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
+        
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div>
+
+          <span>{this.state.array}</span>
+
+          <input onChange={ (e) => this.handleUserInput(e.target.value)} />
+
+          <button onClick={ () => this.handleFilter(this.state.userInput)}>Filter</button>
+
+          <span>{this.state.filteredArray}</span>
+
+          <button onClick={ () => this.handleClear()}>Clear</button>
+
+        </div>
+
       </div>
     );
   }
