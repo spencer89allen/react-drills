6225a7e06b593041f534f37539e44e0aa3edede2
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-01-28 18:13:05 -0700

    app-4 complete

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -2,17 +2,82 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './Components/Button'
+
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      userName: '',
+      password: '',
+      loggedIn: false,
+    }
+
+    this.handleName = this.handleName.bind(this)
+    this.handlePassword = this.handlePassword.bind(this)
+    this.handleLogin = this.handleLogin.bind(this)
+
+  }
+
+  handleName(e) {
+    this.setState({
+      userName: e,
+    })
+  }
+
+  handlePassword(e) {
+    this.setState({
+      password: e,
+    })
+  }
+
+  handleLogin() {
+
+    var login = false
+    console.log(this.state.userName)
+    console.log(this.state.password)
+    if(this.state.userName === "spencer" && this.state.password === 'password') {
+      login = true
+      alert("Welcome")
+    }
+    else {
+      login = false
+      alert("Go away")
+    }
+
+    console.log(this.state.userName)
+    console.log(this.state.password)
+    console.log(this.state.loggedIn)
+
+    this.setState({
+      loggedIn: login,
+    })
+
+  }
+
+
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div>
+
+          <input onChange={ (e) => this.handleName(e.target.value)}></input>
+
+          <input onChange={ (e) => this.handlePassword(e.target.value)}></input>
+
+          <Button action={this.handleLogin}>Login</Button>
+
+        </div>
+
       </div>
     );
   }
---
+++ app-4/src/Components/Button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default function Button(props) {
+
+
+
+    return(
+
+        <button onClick={props.action}>
+            {props.children}
+        </button>
+
+    )
+
+}
