85f6124dd0bf3f15cfbcc01add8c17d911cb19a2
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-01-30 16:27:36 -0700

    app-5 complete

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -2,17 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Image from './Components/Image'
+
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      input: '',
+      image: ''
+    }
+
+    this.handleInput = this.handleInput.bind(this)
+    this.handlePost = this.handlePost.bind(this)
+
+  }
+
+  handleInput(e) {
+
+    this.setState({
+      input: e,
+    })
+
+  }
+
+  handlePost() {
+
+    this.setState({
+      image: this.state.input,
+    })
+
+  }
+
+
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div>
+
+          <input onChange={ (e) => this.handleInput(e.target.value)}></input>
+
+          <button onClick={ () => this.handlePost(this.state.input)}>Post</button>
+
+          <Image image={ this.state.image }></Image>
+        
+        </div>
+
       </div>
     );
   }
---
+++ app-5/src/Components/Image.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export default function image (props) {
+
+    return (
+        <div>
+            <img src={props.image} alt='cannot be displayed'></img>
+        </div>
+    )
+    
+}
